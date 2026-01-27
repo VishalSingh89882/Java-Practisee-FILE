8de51f504963e12c016d4923709b84630ec90ed1
Vishal <vishal.singh@nextuple.com>
2026-01-27 13:46:43 +0530

    added files

--- src/secondPackage/D.java
+++ src/secondPackage/D.java
@@ -9,10 +9,13 @@ public class D extends B {
         a.publicVariableInA = 60;
         //a.protectedVariableInA = 20;
         //a.printMessageFromA();
+        // a.defaultVariableInA = 400;
+
 
         B b = new B();
         b.publicVariableInB = 60;
         printMessageFromB();
         protectedVariableInB = 100;
+       // b.defaultVariableInB = 50;
     }
 }
