5f67a801146f43bf76ab93e07e0d37176a59f5f9
Vishal <vishal.singh@nextuple.com>
2026-01-27 11:06:59 +0530

    added default modifiers file

---
+++ src/firstpackage/A.java
@@ -0,0 +1,20 @@
+
+
+package firstpackage;
+
+/*
+Public > Protected > Default > Private
+
+ */
+
+public class A {
+    private int privateVariableInA;
+    int defaultVariableInA;
+    public int publicVariableInA;
+    protected int protectedVariableInA;
+
+    protected void printMessageFromA(){
+        privateVariableInA = 1000;
+        protectedVariableInA = 100;
+    }
+}
